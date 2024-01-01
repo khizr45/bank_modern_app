45b48d9ed723abddd63cc297239ce9e06fbb418f
khizr45 <79244644+khizr45@users.noreply.github.com>
2024-01-01 20:35:59 +0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,6 @@ const Hero = () => {
             Account
           </p>
         </div>
-
         <div className='flex flex-row justify-between items-center w-full'>
           <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
             The Next <br /> {" "}
